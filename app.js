42a181f31e53879a062ea919dd344b433f3207d8
Andy Wang <AndrewWang996@gmail.com>
2016-01-11 02:53:22 -0500

    using dotenv to configure environment variables + errorHandler

--- app.js
+++ app.js
@@ -4,11 +4,37 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var dotenv = require('dotenv');
+var mongoose = require('mongoose');
+
+
+/**
+ * Load environment variables from .env file, where API keys and passwords are configured.
+ *
+ * Default path: .env
+ */
+dotenv.load({ path: '.env' });
+
+/**
+ * Controllers (route handlers).
+ */
+var homeController = require('./routes/home');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+
 var app = express();
+
+/**
+ * Connect to MongoDB.
+ */
+mongoose.connect(process.env.MONGODB || process.env.MONGOLAB_URI);
+mongoose.connection.on('error', function() {
+    console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
+    process.exit(1);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,39 +48,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+// app.use('/', routes);
+// app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+app.get('/', homeController.index);
 
-// error handlers
+/**
+ * Error Handler.
+ */
+app.use(errorHandler());
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
 
 
 module.exports = app;
