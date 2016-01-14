e1faef9fd99f64f2c8b9987845903518c3b0aadd
Jimmy <jimmy@dhcp-18-189-112-26.dyn.MIT.EDU>
2016-01-14 01:50:43 -0500

    Got photos from public Facebook albums... the code is short, but took a long time to figure out... also we need the user_photos permission or we can only use public albums

--- controllers/scrapbook.js
+++ controllers/scrapbook.js
@@ -1,18 +1,54 @@
 var graph;
 
 var _ = require('lodash');
-var async = require('async');
-var querystring = require('querystring');
+var Promise = require('bluebird');
 
 /**
  * GET /scrapbook
  * Scrapbook page, only accessible if user is logged in through facebook
  */
 exports.getIndex = function(req, res) {
-    graph = require('fbgraph');
+    graph = Promise.promisifyAll(require('fbgraph'));
 
     var token = _.find(req.user.tokens, { kind: 'facebook' });
     graph.setAccessToken(token.accessToken);
+
+// We can only use public albums unless we get the user_photos permission
+var ats = "116850108484293";
+console.log(req.user.facebook);
+    graph.getAsync(ats+"/albums")
+    .then(function(albums) {
+        return Promise.map(albums.data, function(album) {
+            return graph.getAsync("/"+album.id+"/photos");
+        });
+    }).then(function(data) {  
+        return Promise.map(data[0].data, function(photo) {
+            return graph.getAsync("/"+photo.id+"?fields=id,name,picture,source");
+        });
+    }).then(function(data) {
+
+
+
+
+        console.log(data);
+        var photos = data;
+        res.render("scrapbook/index", {
+            title: "Time Capsule Scrapbook",
+            photos: photos
+        });
+    });
+};
+
+
+
+
+
+
+
+
+// I commented out the original stuff on this page
+
+/*
     async.parallel({
             getMe: function(done) {
                 graph.get(req.user.facebook + "?fields=id,name,email,first_name,last_name,gender,link,locale,timezone", function(err, me) {
@@ -37,7 +73,7 @@ exports.getIndex = function(req, res) {
                 friends: results.getMyFriends
             });
         });
-}
+*/
 
 
 
